369de4d3d35297b1f8dd7fd6943e9a0062733df4
Laja12 <103024852+Laja12@users.noreply.github.com>
2025-04-22 15:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "vpc" {
   name   = "eks-fargate"
   cidr   = "10.0.0.0/16"
   private_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  azs = ["us-east-1a", "us-east-1b"]
+  azs = ["us-west-1a", "us-west-1b"]
 }
 
 module "iam" {
